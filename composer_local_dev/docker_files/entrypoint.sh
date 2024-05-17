f1b575510fcd0b65b06c356a491faedf70ee471d
Christian Kravanja <christian.kravanja@algolia.com>
2024-05-17 11:27:30 +0200

    fix

--- composer_local_dev/docker_files/entrypoint.sh
+++ composer_local_dev/docker_files/entrypoint.sh
@@ -51,7 +51,9 @@ read -r -d '' PATCH << EOL
    {%- endset -%}
 EOL
 
-cd / && patch -p0 <<< "$PATCH"
+# cd / && patch -p0 <<< "$PATCH"
+cd /
+echo "$PATCH" |patch -p0
 
 export PATH="$PATH:/home/airflow/docker_files/bin"
 
